8fad6921aaf9d36cdf92ca17ad96308e422c6336
Okmorabito <166557250+Okmorabito@users.noreply.github.com>
2024-04-09 17:08:03 -0400

    Added 2nd LED

--- ledBlink.ino
+++ ledBlink.ino
@@ -1,12 +1,16 @@
 int ledPin = 13;
+int ledPin2= 14;
 
 void setup() {
   pinMode(ledPin, OUTPUT);
+  pinMode(ledPin, OUTPUT); 
 }
 
 void loop() {
   digitalWrite(ledPin, HIGH);
-  delay(250);
+  delay(500);
+  digitalWrite(ledPin2, HIGH);
   digitalWrite(ledPin, LOW);
   delay(250);
+  digitalWrite(ledPin2, LOW);
 }
